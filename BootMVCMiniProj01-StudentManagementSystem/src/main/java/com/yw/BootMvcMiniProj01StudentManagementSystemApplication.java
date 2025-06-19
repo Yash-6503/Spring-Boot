d135debb2585c8ba992b39915227edd9913451cc
Yash-6503 <yashwalke6503@gmail.com>
2025-06-19 23:58:31 +0530

    Spring Boot MVC Mini Proj01 - Student Management System

---
+++ BootMVCMiniProj01-StudentManagementSystem/src/main/java/com/yw/BootMvcMiniProj01StudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcMiniProj01StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcMiniProj01StudentManagementSystemApplication.class, args);
+	}
+
+}
